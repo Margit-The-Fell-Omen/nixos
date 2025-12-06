f40eaef0ba3d3ddf1e1cb7727aff709e8c852fd8
Ushki <wartim9494@gmail.com>
2025-12-06 13:24:38 +0300

    star rail theme

--- modules/themes/femboy-theme/default.nix
+++ modules/themes/femboy-theme/default.nix
@@ -5,25 +5,25 @@
     description = "Femboy-theme!!!";
     polarity = "dark";
 
-    liveWallpaper = "3578156286";
+    #liveWallpaper = "3578156286";
 
     background = ./wallpaper.jpg;
     backgroundSha256 = "";
 
-    base00 = "191724"; # Surface (Background)
-    base01 = "1f1d2e"; # Surface overlay
-    base02 = "26233a"; # Surface highlight
-    base03 = "6e6a86"; # Muted (Comments)
-    base04 = "908caa"; # Subtle
-    base05 = "e0def4"; # Text (Foreground)
-    base06 = "e0def4"; # Text
-    base07 = "524f67"; # Highlight low
-    base08 = "eb6f92"; # Love (Red/Pink)
-    base09 = "f6c177"; # Gold (Orange)
-    base0A = "ebbcba"; # Rose (Yellow/Pinkish)
-    base0B = "31748f"; # Pine (Green/Teal)
-    base0C = "9ccfd8"; # Foam (Cyan)
-    base0D = "c4a7e7"; # Iris (Blue/Purple)
-    base0E = "c4a7e7"; # Iris (Purple)
-    base0F = "524f67"; # Highlight high
+    base00 = "161014";
+    base01 = "211419";
+    base02 = "361f27";
+    base03 = "5c3a46";
+    base04 = "8f5e70";
+    base05 = "ff7eb6";
+    base06 = "ffb3d1";
+    base07 = "ffe6f0";
+    base08 = "ff2e63";
+    base09 = "ff85a1";
+    base0A = "fcc2d7";
+    base0B = "d45785";
+    base0C = "e08dbd";
+    base0D = "f06292";
+    base0E = "c75c93";
+    base0F = "9e4566";
 }
